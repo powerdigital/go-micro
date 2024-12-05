4846362c4ab8ec16b2dba1c9da13d80ef4efe1e0
Dilyaver Saliev <powerdigital2010@gmail.com>
2024-12-06 01:57:28 +0300

    change grpc handlers place

--- internal/build/grpc.go
+++ internal/build/grpc.go
@@ -6,6 +6,7 @@ import (
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 
+	grpcv1 "github.com/powerdigital/go-micro/internal/transport/grpc/v1"
 	greeterv1 "github.com/powerdigital/go-micro/pkg/greeter/v1"
 )
 
@@ -26,4 +27,13 @@ func (b *Builder) GRPCServer() (*grpc.Server, error) {
 	})
 
 	return grpcServer, nil
+}
+
+func (b *Builder) GreetingServer() (*grpcv1.GreetingServer, error) {
+	service, err := b.GreetingService()
+	if err != nil {
+		return nil, errors.Wrap(err, "build service")
+	}
+
+	return grpcv1.NewGreetingServer(service), nil
 }
